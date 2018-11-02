ff60fcee53b76d6abfc41dccaed41a38744c2181
tgray017 <tgray017@gmail.com>
2018-11-02 20:08:31 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,8 +10,8 @@ class Song
   def self.new_by_filename(file_name)
     name = file_name.split(" - ")[1]
     song = self.new(name)
-    binding.pry
     artist_1 = file_name.split(" - ")[0]
+        binding.pry
     song.artist = Artist.find_or_create_by_name(artist_1)
   end
   
