228789d4d46021b5bf62da9c3e5ccc7182490661
tgray017 <tgray017@gmail.com>
2018-11-02 20:47:42 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,18 +16,4 @@ class Song
     artist.add_song(song)
     song
   end
-  
 end
-
-
-file_name = "Michael Jackson - Black or White - pop.mp3"
-new_instance = Song.new_by_filename(file_name)
-# song.artist = artist
-
- # def self.new_by_filename(file_name)
-#    song = self.new(file_name.split(" - ")[1])
-#    song.artist = Artist.find_or_create_by_name(file_name.split(" - ")[0])
-#    song.artist.save
-#    song.artist.add_song(self)
-#    song
-#  end
